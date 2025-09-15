053f575261d2541510990385b60c7f5ec338b6be
David J. Shepard II <daveman1010220@gmail.com>
2025-09-15 17:33:46 +0000

    first cut at flake automation for sed-key

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "sed-key: minimal OPAL unlock tool";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # or your preferred channel
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -11,7 +11,7 @@
       let
         pkgs = import nixpkgs { inherit system; };
       in {
-        packages.default = pkgs.rustPlatform.buildRustPackage rec {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "sed-key";
           version = "0.1.0";
 
@@ -19,18 +19,43 @@
 
           cargoLock.lockFile = ./Cargo.lock;
 
-          # If you want to ensure libclang is available at build time:
-          nativeBuildInputs = with pkgs; [ pkg-config llvmPackages.libclang ];
+          # bring libclang + llvm-config into the sandbox
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            llvmPackages.libclang
+            llvmPackages.llvm
+            linuxHeaders
+          ];
 
-          # Optional meta info:
+          # explicitly tell clang-sys where to find stuff
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+          LLVM_CONFIG_PATH = "${pkgs.llvmPackages.llvm.dev}/bin/llvm-config";
+
+          # Add kernel header path to bindgen
+          BINDGEN_EXTRA_CLANG_ARGS = "-I${pkgs.linuxHeaders}/include";
+
           meta = with pkgs.lib; {
             description = "Minimal Rust tool to unlock NVMe OPAL SED drives";
-            homepage = "https://github.com/yourname/sed-key";
+            homepage = "https://github.com/daveman1010221/sed-key";
             license = licenses.mit;
-            maintainers = [ maintainers.yourname ];
             platforms = platforms.linux;
           };
         };
+
+        # Dev shell for hacking
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            pkg-config
+            llvmPackages.libclang
+            llvmPackages.llvm
+            linuxHeaders
+          ];
+
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+          LLVM_CONFIG_PATH = "${pkgs.llvmPackages.llvm.dev}/bin/llvm-config";
+          BINDGEN_EXTRA_CLANG_ARGS = "-I${pkgs.linuxHeaders}/include";
+        };
       });
 }
-
